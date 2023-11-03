12ee4c39e04d9cf71fcfcdd662270161759becac
Helton Barbosa <heltonbs@gmail.com>
2023-11-03 06:33:49 -0300

    Uso de docstring

--- secao_03/aula_001.py
+++ secao_03/aula_001.py
@@ -2,3 +2,18 @@
 print(123)
 # Utilizamos o caractere # para fazer comentários que não serão lidos.
 # O comentário é apenas uma linha por vez.
+
+# docstring - não é comentário. Docstring em Python é uma convenção usada para
+# documentar funções, métodos, classes e módulos de forma a tornar o código
+# mais legível e compreenvível para o desenvolvedor
+
+
+def somar(a, b):
+    """
+    Esta função recebe dois números, a e b, e retorna a soma deles.
+
+    :param a: O primeiro número.
+    :param b: O segundo número.
+    :return: A soma de a e b.
+    """
+    return a + b
