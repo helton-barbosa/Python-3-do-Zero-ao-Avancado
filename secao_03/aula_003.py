af22b29961ce7df9f5d085bd617a526056a550d9
Helton Barbosa <heltonbs@gmail.com>
2023-11-06 06:04:05 -0300

    Tipo de dado - String

---
+++ secao_03/aula_003.py
@@ -0,0 +1,13 @@
+# Tipo de dado String
+
+# aspas simples
+print('Esse é um exemplo usando aspas simples')
+
+# aspas duplas
+print("Esse é um exemplo usando aspas duplas")
+
+# escape
+print("Ela disse: \"Adeus!\"")
+
+# raw string - r
+print(r'C:\Program Files\Python')
