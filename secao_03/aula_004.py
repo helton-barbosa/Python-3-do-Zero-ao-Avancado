6cf9574e4a56527385cb387acbd661903cc3a9ed
Helton Barbosa <heltonbs@gmail.com>
2023-11-07 06:36:32 -0300

    Tipos int e float; class type()

---
+++ secao_03/aula_004.py
@@ -0,0 +1,11 @@
+# Tipo de dados int e float
+
+# int
+print(10, type(10))
+print(-10, type(-10))
+print(0, type(0))
+
+# float
+print(10.25, type(10.25))
+print(-10.25, type(-10.25))
+print(0.0, type(0.0))
