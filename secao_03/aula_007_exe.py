aef08f370730e72b7e51f2ac587be4a1a74e6e13
Helton Barbosa <heltonbs@gmail.com>
2023-11-18 12:46:27 -0300

    Exercício com variáveis e tipos de dados

---
+++ secao_03/aula_007_exe.py
@@ -0,0 +1,13 @@
+nome = 'Helton'
+sobrenome = 'Barbosa Santos Ferreira'
+idade = 38
+ano_nascimento = 1985
+altura_metros = 1.79
+maior_de_idade = idade >= 18
+
+print(f'Nome: {nome}')
+print(f'Sobrenome: {sobrenome}')
+print(f'Idade: {idade}')
+print(f'Ano de nascimento: {ano_nascimento}')
+print(f'É maior de idade? {maior_de_idade}')
+print(f'Altura em metros: {altura_metros}')
