70036d849f21e8223cc452448b1d4775eeb8190f
Helton Barbosa <heltonbs@gmail.com>
2023-11-18 10:27:22 -0300

    Variáveis em Python

---
+++ secao_03/aula_007.py
@@ -0,0 +1,11 @@
+# Variáveis em Python
+
+meu_nome = 'Helton'
+meu_sobrenome = 'Barbosa Santos Ferreira'
+minha_idade = '38'
+soma_de_2_mais_2 = 2 + 2
+_apelido = 'Lampião'
+
+print(f'Meu nome é {meu_nome} {meu_sobrenome} e tenho {minha_idade} anos.')
+print(f'Alguns amigos me chama de {_apelido}.')
+print(f'O resultado da soma é {soma_de_2_mais_2}.')
